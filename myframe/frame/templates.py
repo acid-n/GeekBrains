1272fc5783c89487476f0acb81caf8e5da7ae0af
Nikolay <acid_n@mail.ru>
2021-02-18 21:49:57 +0300

    Update MyFrame: add contact page

--- myframe/frame/templates.py
+++ myframe/frame/templates.py
@@ -4,16 +4,15 @@ import os
 
 def render(template_name, folder='templates', **kwargs):
     """
-
     :param template_name: имя шаблона
     :param folder: папка в которой ищем шаблон
     :param kwargs: параметры
     :return:
     """
     file_path = os.path.join(folder, template_name)
-    # открываем шаблон по имени
+    # Открываем шаблон по имени
     with open(file_path, encoding='utf-8') as f:
-        # читаем
+        # Читаем
         template = Template(f.read())
     # рендерим шаблон с параметрами
     return template.render(**kwargs)
